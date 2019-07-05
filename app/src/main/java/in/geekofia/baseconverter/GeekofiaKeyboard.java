9f1360568351d2ea9c6f7c9fbd80928ca097d3dd
chankruze <chankruze@gmail.com>
2019-07-06 00:01:03 +0530

    Initial Commit
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ app/src/main/java/in/geekofia/baseconverter/GeekofiaKeyboard.java
@@ -0,0 +1,95 @@
+package in.geekofia.baseconverter;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+import android.widget.LinearLayout;
+
+import com.google.android.material.button.MaterialButton;
+
+public class GeekofiaKeyboard extends LinearLayout implements View.OnClickListener {
+
+    private MaterialButton button1, button2, button3, button4,
+            button5, button6, button7, button8,
+            button9, button0, buttonDelete, buttonEnter;
+
+    private SparseArray<String> keyValues = new SparseArray<>();
+    private InputConnection inputConnection;
+
+    public GeekofiaKeyboard(Context context) {
+        this(context, null, 0);
+    }
+
+    public GeekofiaKeyboard(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public GeekofiaKeyboard(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        LayoutInflater.from(context).inflate(R.layout.keyboard, this, true);
+        button1 = findViewById(R.id.button_1);
+        button1.setOnClickListener(this);
+        button2 = findViewById(R.id.button_2);
+        button2.setOnClickListener(this);
+        button3 = findViewById(R.id.button_3);
+        button3.setOnClickListener(this);
+        button4 = findViewById(R.id.button_4);
+        button4.setOnClickListener(this);
+        button5 = findViewById(R.id.button_5);
+        button5.setOnClickListener(this);
+        button6 = findViewById(R.id.button_6);
+        button6.setOnClickListener(this);
+        button7 = findViewById(R.id.button_7);
+        button7.setOnClickListener(this);
+        button8 = findViewById(R.id.button_8);
+        button8.setOnClickListener(this);
+        button9 = findViewById(R.id.button_9);
+        button9.setOnClickListener(this);
+        button0 = findViewById(R.id.button_0);
+        button0.setOnClickListener(this);
+        buttonDelete = findViewById(R.id.button_delete);
+        buttonDelete.setOnClickListener(this);
+
+        keyValues.put(R.id.button_1, "1");
+        keyValues.put(R.id.button_2, "2");
+        keyValues.put(R.id.button_3, "3");
+        keyValues.put(R.id.button_4, "4");
+        keyValues.put(R.id.button_5, "5");
+        keyValues.put(R.id.button_6, "6");
+        keyValues.put(R.id.button_7, "7");
+        keyValues.put(R.id.button_8, "8");
+        keyValues.put(R.id.button_9, "9");
+        keyValues.put(R.id.button_0, "0");
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (inputConnection == null)
+            return;
+
+        if (view.getId() == R.id.button_delete) {
+            CharSequence selectedText = inputConnection.getSelectedText(0);
+
+            if (TextUtils.isEmpty(selectedText)) {
+                inputConnection.deleteSurroundingText(1, 0);
+            } else {
+                inputConnection.commitText("", 1);
+            }
+        } else {
+            String value = keyValues.get(view.getId());
+            inputConnection.commitText(value, 1);
+        }
+    }
+
+    public void setInputConnection(InputConnection ic) {
+        inputConnection = ic;
+    }
+}
---
+++ app/src/main/java/in/geekofia/baseconverter/MainActivity.java
@@ -0,0 +1,138 @@
+package in.geekofia.baseconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.InputType;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Spinner mFromBaseSpinner, mToBaseSpinner;
+    TextInputLayout mInputNumber, mConvertedNumber;
+    TextInputEditText mInputNumberEditText,mConvertedNumberEditText;
+    MaterialButton mButtonReset, mButtonConvert;
+    private int mSelectedBaseFrom, mSelectedBaseTo;
+    private GeekofiaKeyboard keyboard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+
+        mFromBaseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String baseSplitFrom[] = mFromBaseSpinner.getSelectedItem().toString().split(" ");
+                mSelectedBaseFrom = Integer.parseInt(baseSplitFrom[0]);
+
+                if (mSelectedBaseFrom == 16){
+                    mInputNumberEditText.setInputType(InputType.TYPE_CLASS_TEXT);
+                    mInputNumberEditText.setTextIsSelectable(true);
+
+                    mInputNumberEditText.setShowSoftInputOnFocus(false);
+                    InputConnection ic = mInputNumberEditText.onCreateInputConnection(new EditorInfo());
+                    keyboard.setInputConnection(ic);
+                }else {
+                    mInputNumberEditText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    mInputNumberEditText.setTextIsSelectable(true);
+                    mInputNumberEditText.setShowSoftInputOnFocus(false);
+
+                    InputConnection ic = mInputNumberEditText.onCreateInputConnection(new EditorInfo());
+                    keyboard.setInputConnection(ic);
+                }
+
+                String mHintFromBase = baseSplitFrom[1];
+                mHintFromBase = mHintFromBase.replace("(","");
+                mHintFromBase = mHintFromBase.replace(")","");
+                mInputNumber.setHint(mHintFromBase);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        mToBaseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String baseSplitTo[] = mToBaseSpinner.getSelectedItem().toString().split(" ");
+                mSelectedBaseTo = Integer.parseInt(baseSplitTo[0]);
+
+                String mHintToBase = baseSplitTo[1];
+                mHintToBase = mHintToBase.replace("(","");
+                mHintToBase = mHintToBase.replace(")","");
+                mConvertedNumber.setHint(mHintToBase);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        mButtonReset.setOnClickListener(this);
+        mButtonConvert.setOnClickListener(this);
+    }
+
+    private void initViews() {
+        // From Base Spinner
+        mFromBaseSpinner = findViewById(R.id.spinner_from);
+        ArrayAdapter<CharSequence> mFromAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_select_base, android.R.layout.simple_spinner_item);
+        mFromAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mFromBaseSpinner.setAdapter(mFromAdapter);
+
+        // To Base Spinner
+        mToBaseSpinner = findViewById(R.id.spinner_to);
+        ArrayAdapter<CharSequence> mToAdapter = ArrayAdapter.createFromResource(this,
+                R.array.array_select_base, android.R.layout.simple_spinner_item);
+        mToAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mToBaseSpinner.setAdapter(mToAdapter);
+
+        mInputNumber = findViewById(R.id.input_number);
+        mConvertedNumber = findViewById(R.id.converted_number);
+
+        mButtonReset = findViewById(R.id.button_reset);
+        mButtonConvert = findViewById(R.id.button_convert);
+
+        mInputNumberEditText = findViewById(R.id.input_number_edit_text);
+        mInputNumberEditText.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+
+        mConvertedNumberEditText = findViewById(R.id.converted_number_edit_text);
+
+        keyboard = findViewById(R.id.keyboard);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button_reset:
+                 mInputNumberEditText.setText("");
+                 mConvertedNumberEditText.setText("");
+
+                break;
+            case R.id.button_convert:
+                String test = mInputNumberEditText.getText().toString();
+                mConvertedNumberEditText.setText(test);
+                break;
+        }
+    }
+
+
+}
